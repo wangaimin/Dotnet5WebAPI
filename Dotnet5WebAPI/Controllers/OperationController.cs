7310e7d1e200393a707716b5de81c796e948950d
wangaimin <448497795@qq.com>
2022-04-20 07:14:32 +0800

    add todo

---
+++ Dotnet5WebAPI/Controllers/OperationController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dotnet5WebAPI.Controllers
+{
+    public class OperationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //todo:调用瞬时服务、范围服务、单例服务
+    }
+}
